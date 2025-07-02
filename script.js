944154358bb7c067980504cbdb76bb0695d69fcd
khaya05 <khayamnyandu5@gmail.com>
2025-07-02 11:34:12 +0200

    Add .gitignore, update activity selection and form handling

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('#activity-form');
 const activities = document.querySelector('#activities');
-const activityType = document.querySelector('#activity-select');
+const activitiesSelect = document.querySelector('#activity-select');
 const distance = document.querySelector('#distance');
 const submitBtn = document.querySelector('#submit');
 const category = document.querySelector('#category');
@@ -12,37 +12,125 @@ let editID = '';
 
 form.addEventListener('submit', (e) => addActivity(e));
 
-// clear items event listener
-//
+// clear items event listener btn
+
+activitiesSelect.addEventListener('change', () => {
+  const selectedOption =
+    activitiesSelect.options[activitiesSelect.selectedIndex];
+  const cat = selectedOption.parentElement.label;
+  category.value = cat;
+});
+
+window.addEventListener('DOMContentLoaded', displayActivities);
 
 function addActivity(e) {
   e.preventDefault();
-  const activityValue = activityType.value;
+
+  const activityValue = activitiesSelect.value;
   const distanceValue = Number(distance.value);
   const { icon, altText, units } = getIcon(category.value);
   const id = new Date().getTime().toString();
 
   const amount = calculateEmissions(category.value, distanceValue);
 
-  console.log({
-    amount,
-    category: category.value,
-    distanceValue,
-    activityValue,
+  if (activityValue && !editFlag) {
+    createListItem(
+      id,
+      icon,
+      altText,
+      activityValue,
+      distanceValue,
+      units,
+      amount,
+      category.value
+    );
+
+    // add to local storage
+    addToLocalStorage(
+      id,
+      icon,
+      altText,
+      activityValue,
+      distanceValue,
+      units,
+      amount,
+      category.value
+    );
+  } else if (activityValue && editFlag) {
+    editElement.innerHTML = populateListItem(
+      icon,
+      altText,
+      activityValue,
+      distanceValue,
+      units,
+      amount,
+      category.value
+    );
+    showNotification('Activity updated successfully', 'success');
+    editLocalStorage(
+      editID,
+      icon,
+      altText,
+      activityValue,
+      distanceValue,
+      units,
+      amount,
+      category.value
+    );
+  } else {
+    showNotification('Please select an activity', 'error');
+  }
+
+  resetForm();
+}
+
+function createListItem(
+  id,
+  icon,
+  altText,
+  activityValue,
+  distanceValue,
+  units,
+  amount,
+  category
+) {
+  const listItem = document.createElement('li');
+  const attr = document.createAttribute('data-id');
+  attr.value = id;
+  listItem.setAttributeNode(attr);
+
+  listItem.innerHTML = populateListItem(
     icon,
     altText,
+    activityValue,
+    distanceValue,
     units,
-  });
+    amount,
+    category
+  );
 
-  if (activityValue && !editFlag) {
-    const listItem = document.createElement('li');
-    const attr = document.createAttribute('data-id');
-    attr.value = id;
-    listItem.setAttributeNode(attr);
+  activities.appendChild(listItem);
+  showNotification('Activity added successfully', 'success');
 
-    listItem.innerHTML = `<div class="left">
+  const deleteBtn = listItem.querySelector('.delete-btn');
+  const editBtn = listItem.querySelector('.edit-btn');
+
+  deleteBtn.addEventListener('click', (e) => deleteActivity(e));
+  editBtn.addEventListener('click', (e) => editActivity(e));
+}
+
+function populateListItem(
+  icon,
+  altText,
+  activityValue,
+  distanceValue,
+  units,
+  amount,
+  category
+) {
+  return `<div class="left">
         <img src="./assets/${icon}" alt="${altText}">
-        <span class="activity">${activityValue} - <span class="amount"> ${distanceValue}${units}</span></span>
+        <span class="activity" data-category="${category}">${activityValue} - ${distanceValue}${units}</span>
       </div>
 
       <span class="co2-amount">${amount} kg CO<span class="symbol">2</span></span>
@@ -56,27 +144,6 @@ function addActivity(e) {
           </button>
       </div>
    `;
-
-    activities.appendChild(listItem);
-    showNotification('Activity added successfully', 'success');
-
-    const deleteBtn = listItem.querySelector('.delete-btn');
-    const editBtn = listItem.querySelector('.edit-btn');
-
-    deleteBtn.addEventListener('click', (e) => deleteActivity(e));
-    editBtn.addEventListener('click', (e) => editActivity(e));
-
-    // add to local storage
-    // addToLocalStorage(id, )
-
-    // reset form
-    resetForm();
-  } else if (activityValue && editFlag) {
-    // update activity
-    console.log('update activity');
-  } else {
-    showNotification('Please select an activity', 'error');
-  }
 }
 
 const notification = document.querySelector('.notification');
@@ -95,11 +162,11 @@ function showNotification(message, type) {
 function getIcon(category) {
   switch (category) {
     case 'transport':
-      return { icon: 'car.svg', altText: 'Car icon', units: ' km' };
+      return { icon: 'car.svg', altText: 'transport icon', units: ' km' };
     case 'food':
-      return { icon: 'knife-fork.svg', altText: 'Food icon', units: ' g' };
+      return { icon: 'knife-fork.svg', altText: 'food icon', units: ' g' };
     case 'energy':
-      return { icon: 'bolt.svg', altText: 'Energy icon', units: ' kWh' };
+      return { icon: 'bolt.svg', altText: 'energy icon', units: ' kWh' };
   }
 }
 
@@ -120,41 +187,108 @@ function calculateEmissions(category, amount) {
 }
 
 // ***** LOCAL STORAGE FUNCTIONS *****
-function addToLocalStorage(id, activity) {
-  console.log('saved to local storage');
+function getLocalStorage() {
+  return localStorage.getItem('activities')
+    ? JSON.parse(localStorage.getItem('activities'))
+    : [];
+}
+
+function setLocalStorage(activities) {
+  localStorage.setItem('activities', JSON.stringify(activities));
+}
+
+function addToLocalStorage(
+  id,
+  icon,
+  altText,
+  activityValue,
+  distanceValue,
+  units,
+  amount,
+  category
+) {
+  const activity = {
+    id,
+    icon,
+    altText,
+    activityValue,
+    distanceValue,
+    units,
+    amount,
+    category,
+  };
+
+  let activities = getLocalStorage();
+
+  activities.push(activity);
+
+  setLocalStorage(activities);
 }
 
 function removeFromLocalStorage(id) {
-  console.log('removed from local storage');
+  let activities = getLocalStorage();
+  activities = activities.filter((activity) => activity.id !== id);
+  setLocalStorage(activities);
 }
 
 function resetForm() {
-  activityType.value = '';
+  activitiesSelect.value = '';
   distance.value = '';
   category.value = 'n/a';
   editFlag = false;
   editID = '';
-  submitBtn.textContent = '+ Add Activity'
+  submitBtn.textContent = '+ Add Activity';
+}
+
+function editLocalStorage(
+  id,
+  icon,
+  altText,
+  activityValue,
+  distanceValue,
+  units,
+  amount,
+  category
+) {
+  let activities = getLocalStorage();
+
+  const updatedActivities = activities.map((activity) => {
+    if (activity.id === id) {
+      return {
+        id,
+        icon,
+        altText,
+        activityValue,
+        distanceValue,
+        units,
+        amount,
+        category,
+      };
+    }
+    return activity;
+  });
+
+  setLocalStorage(updatedActivities);
 }
 
 // ***** EDIT AND DELETE FUNCTIONS *****
 function editActivity(e) {
   const element = e.currentTarget.parentElement.parentElement;
+  const activity = element.querySelector('.activity');
+  editElement = element;
+
   let currId = element.dataset.id;
-  let [currActivity, amountInput] = element
-    .querySelector('.activity')
-    .innerHTML.split(' - ');
+  let [currActivity, currAmount] = activity.innerHTML.split(' - ');
 
-  let currEmission = element
-    .querySelector('.amount')
-    .innerHTML.match(/[\d.]+(?=\s*\w+)/)[0];
+  let currEmission = currAmount.match(/[\d.]+(?=\s*\w+)/)[0];
 
-  activityType.value = currActivity;
+  activitiesSelect.value = currActivity;
   distance.value = Number(currEmission);
+  category.value = activity.dataset.category;
 
   editFlag = true;
   editID = currId;
-  submitBtn.textContent = 'Update Activity'
+  submitBtn.textContent = 'Update Activity';
 }
 
 function deleteActivity(e) {
@@ -188,4 +322,36 @@ function clearActivities() {
   showNotification('All activities cleared', 'success');
   resetForm();
   // clear local storage
+}
+
+function displayActivities() {
+  let activities = getLocalStorage();
+
+  if (activities.length > 0) {
+    activities.forEach((activity) => {
+      const {
+        id,
+        icon,
+        altText,
+        activityValue,
+        distanceValue,
+        units,
+        amount,
+        category,
+      } = activity;
+      createListItem(
+        id,
+        icon,
+        altText,
+        activityValue,
+        distanceValue,
+        units,
+        amount,
+        category
+      );
+    });
+  } else {
+    // show p
+    console.log('list empty');
+  }
 }
